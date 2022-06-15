6c9411f3254cff6bd9d9b78400ea5963eb99b9dc
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-06-15 07:37:35 +0530

    test1

---
+++ ApiFlood/routes/floods.js
@@ -0,0 +1,178 @@
+const express=require('express')
+const router=express.Router();
+const model=require('../models/model')
+
+
+router.get('/',(req,res)=>{
+  model.find().limit(1).then(e=>{
+    res.send(e);
+  }).catch(err=>{
+    console.log(err);
+   })
+})
+
+router.get('/query', (req, res, next) => {
+
+    if (req.query.id) {
+        const id = req.query.id;
+
+      model.findById(id)
+            .then(data => {
+                if (!data) {
+                    res.status(404).send(error)
+                    console.log(error);
+                } else {
+                    res.send(data)
+                }
+            })
+            .catch(err => {
+                res.status(500).send({ message: "Erro retrieving flood with id " + id })
+            })
+
+ 
+
+    } else {
+       model.find()
+            .then(flood => {
+                res.send(flood)
+            })
+            .catch(err => {
+                res.status(500).send({ message: err.message || "Error Occurred while retriving flood information" })
+            })
+      
+    }
+});
+// route.put('/api/flood/:id', controller.update);
+router.delete(':id', (req, res ) => {
+    const id = req.params.id;
+
+   model.findByIdAndDelete(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({ message: `Cannot Delete with id ${id}. Maybe id is wrong` })
+            } else {
+                res.send({
+                    message: "Flood was deleted successfully!"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Flood with id=" + id
+            });
+        });
+       
+     
+});
+//Link
+ 
+ router.get('/link',(req, res ) => {
+
+    if (req.query.id) {
+        const id = req.query.id;
+
+        
+       weblinkmodel.findById(id)
+            .then(link => {
+                if (!link) {
+                    res.status(404).send(errorr)
+                    console.log(errorr);
+                } else {
+                    res.send(link)
+                }
+            })
+            .catch(errr => {
+                res.status(500).send({ message: "Erro retrieving weblink with id " + id })
+            })
+
+    } else {
+     weblinkmodel.find()
+            .then(links=> {
+                res.send(links)
+            })
+            .catch(err => {
+                res.status(500).send({ message: err.message || "Error Occurred while retriving link information" })
+            })
+     
+    }
+
+
+});
+ 
+router.delete('/link/:id', (req, res ) => {
+    const id = req.params.id;
+
+ 
+    weblinkmodel.findByIdAndDelete(id)
+        .then(link => {
+            if (!link) {
+                res.status(404).send({ message: `Cannot Delete with id ${id}. Maybe id is wrong` })
+            } else {
+                res.send({
+                    message: "Link was deleted successfully!"
+                })
+            }
+        })
+        .catch(errr => {
+            res.status(500).send({
+                message: "Could not delete Link with id=" + id
+            });
+        });
+     
+});
+//tweet
+router.get('/twit',(req, res ) => {
+
+    if (req.query.id) {
+        const id = req.query.id;
+
+        
+      twitmodel.findById(id)
+            .then(twit => {
+                if (!twit) {
+                    res.status(404).send(errorr)
+                    console.log(errorr);
+                } else {
+                    res.send(twit)
+                }
+            })
+            .catch(errr => {
+                res.status(500).send({ message: "Erro retrieving weblink with id " + id })
+            })
+
+    } else {
+    twitmodel.find()
+            .then(twits=> {
+                res.send(twits)
+            })
+            .catch(err => {
+                res.status(500).send({ message: err.message || "Error Occurred while retriving link information" })
+            })
+     
+    }
+
+
+});
+ 
+router.delete('/twit/:id', (req, res ) => {
+    const id = req.params.id;
+
+ 
+    twitmodel.findByIdAndDelete(id)
+        .then(twit => {
+            if (!twit) {
+                res.status(404).send({ message: `Cannot Delete with id ${id}. Maybe id is wrong` })
+            } else {
+                res.send({
+                    message: "Twit was deleted successfully!"
+                })
+            }
+        })
+        .catch(errr => {
+            res.status(500).send({
+                message: "Could not delete Twit with id=" + id
+            });
+        });
+     
+});
+module.exports=router
--- ApiFlood/server.js
+++ ApiFlood/server.js
@@ -2,11 +2,14 @@ const express=require("express");
 const app=express()
 const PORT=process.env.PORT || 3000
 const mongoose=require('mongoose')
-require('dotenv')
-mongoose.connect("mongodb+srv://omm:ommanimesh@cluster0.aaovc.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser:true}).then(()=>{
+const Routes=require('./routes/floods')
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true}).then(()=>{
     console.log("db connected successfully");
 })
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/api/floods', Routes)
 app.listen(PORT,()=>{
     console.log("server is up and running");
 })
