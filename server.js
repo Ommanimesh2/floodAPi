27fe05a2066c1e59716fca0b921481f17c7b216b
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-07-09 12:41:38 +0530

    cors error eliminated

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express=require("express");
 const app=express()
-const PORT=process.env.PORT || 3000
+const PORT=process.env.PORT || 7000
 const mongoose=require('mongoose')
 const Routes=require('./routes/floods')
 const Route=require('./routes/flooddata')
+const cors=require('cors')
 require('dotenv').config()
 mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true}).then(()=>{
     console.log("db connected successfully");
 })
-app.use('/api/floods', Routes)
+app.use(cors())
+app.use('/api/floods',cors(), Routes)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept');
-    next();
-});
 app.listen(PORT,()=>{
     console.log("server is up and running");
 })
