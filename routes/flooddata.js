8cce752d2c04e3eca5a2effa13865f7fc76dd09b
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-07-20 22:40:55 +0530

    test21

--- routes/flooddata.js
+++ routes/flooddata.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const model=require('../models/model')
 
 router.get('/',(req,res)=>{
-    model.find({}).limit(1).then(e=>{
+    model.find({}).limit(4).then(e=>{
       res.send(e);
     }).catch(err=>{
       console.log(err);
